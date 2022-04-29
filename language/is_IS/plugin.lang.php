821451b9ecaae371d3bc8309a7797f184d17beff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-29 11:47:35 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Striped  : [s]striped']['/s'] = 'Röndótt  : [s]röndótt[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'Vefslóð: [url=URL]Titill[/url]';
+$lang['Underline : [u]underline']['/u'] = 'Undirstrik : [u]undirstrik[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'Ónúmeraður listi : [ul][li]listi[/li][/ul]';
+$lang['White'] = 'Hvítur';
+$lang['Yellow'] = 'Gulur';
+$lang['huge font'] = 'Mikill';
+$lang['large font'] = 'Stór';
+$lang['normal font'] = 'Venjulegur';
+$lang['small font'] = 'Lítill';
+$lang['tiny font'] = 'Örsmár';
+$lang['Available options'] = 'Mögulegir valkostir';
+$lang['Black'] = 'Svartur';
+$lang['Blue'] = 'Blár';
+$lang['Bold : [b]bold']['/b'] = 'Feitletra : [b]feitletra[/b]';
+$lang['Center : [center]center']['/center'] = 'Miðja : [center]miðja[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'Tölvupóstur : [email]Tölvupóstur[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'Leturlitur : [color=color]texti[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'Leturstærð : [size=X]texti[/size]';
+$lang['Green'] = 'Grænn';
+$lang['Grey'] = 'Grár';
+$lang['Italic : [i]italic']['/i'] = 'Skáletra : [i]skáletra[/i]';
+$lang['List element : [li]element']['/li'] = 'Atriðalisti : [li]listi[/li]';
+$lang['Orange'] = 'Appelsínugulur';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'Númeraður listi : [ol][li]listi[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'Setning : [p]Setning[/p]';
+$lang['Picture : [img]picture']['/img'] = 'Mynd : [img]mynd[/img]';
+$lang['Purple'] = 'Fjólublár';
+$lang['Quote : [quote]quote']['/quote'] = 'Tilvitnun : [quote]tilvitnun[/quote]';
+$lang['Red'] = 'Rauður';
+$lang['Right : [right]right']['/right'] = 'Hægristillt : [right]hægri[/right]';
