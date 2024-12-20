941506e1228ba55fb822caab1fde061dd235dd62
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:07:14 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Available options'] = 'אפשרויות זמינות';
+$lang['Black'] = 'שחור';
+$lang['Blue'] = 'כחול';
+$lang['Bold : [b]bold']['/b'] = 'מודגש : [b]מודגש[/b]';
+$lang['Center : [center]center']['/center'] = 'מרכז : [center]מרכז[/center]';
+$lang['E-mail : [email]Email']['/email'] = 'דוא"ל : [email]דוא"ל[/email]';
+$lang['Font color : [color=color]text']['/color'] = 'צבע גופן : [color=color]טקסט[/color]';
+$lang['Font size : [size=X]text']['/size'] = 'גודל גופן : [size=X]טקסט[/size]';
+$lang['Green'] = 'ירוק';
+$lang['Grey'] = 'אפור';
+$lang['Italic : [i]italic']['/i'] = 'נטוי : [i]נטוי[/i]';
+$lang['List element : [li]element']['/li'] = 'אלמנט רשימה : [li]אלמנט[/li]';
+$lang['Orange'] = 'כתום';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = 'רשימה מסודרת : [ol][li]אלמנט[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = 'פסקה : [p]פסקה[/p]';
+$lang['Picture : [img]picture']['/img'] = 'תמונה : [img]תמונה[/img]';
+$lang['Purple'] = 'סגול';
+$lang['Quote : [quote]quote']['/quote'] = 'ציטוט : [quote]ציטוט[/quote]';
+$lang['Red'] = 'אדום';
+$lang['Right : [right]right']['/right'] = 'ימין : [right]ימין[/right]';
+$lang['Striped  : [s]striped']['/s'] = 'קווים : [s]קווים[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'כתובת URL : [url=URL]כותרת[/url]';
+$lang['Underline : [u]underline']['/u'] = 'קו תחתון : [u]קו תחתון[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = 'רשימה לא מסודרת : [ul][li]אלמנט[/li][/ul]';
+$lang['White'] = 'לבן';
+$lang['Yellow'] = 'צהוב';
+$lang['huge font'] = 'ענק';
+$lang['large font'] = 'גדול';
+$lang['normal font'] = 'רגיל';
+$lang['small font'] = 'קטן';
+$lang['tiny font'] = 'קטנטן';
