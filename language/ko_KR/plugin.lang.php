6960b4afa9419f326baaf92f6f603b4cdb1e225c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-07 19:19:45 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Striped  : [s]striped']['/s'] = '가로줄 : [s]가로줄[/s]';
+$lang['URL : [url=URL]Title']['/url'] = 'URL : [url=URL]제목[/url]';
+$lang['Underline : [u]underline']['/u'] = '밑줄 : [u]밑줄[/u]';
+$lang['Unordered list : [ul][li]element[/li]']['/ul'] = '무순서 목록 : [ul][li]항목[/li][/ul]';
+$lang['White'] = '흰색';
+$lang['Yellow'] = '노란색';
+$lang['huge font'] = '매우 큼';
+$lang['large font'] = '큼';
+$lang['normal font'] = '보통';
+$lang['small font'] = '작음';
+$lang['tiny font'] = '아주 작음';
+$lang['Available options'] = '사용 가능한 옵션';
+$lang['Black'] = '검정';
+$lang['Blue'] = '파랑';
+$lang['Bold : [b]bold']['/b'] = '굵게 : [b]굵게[/b]';
+$lang['Center : [center]center']['/center'] = '중앙 정렬 : [center]중앙 정렬[/center]';
+$lang['E-mail : [email]Email']['/email'] = '이메일 : [email]이메일[/email]';
+$lang['Font color : [color=color]text']['/color'] = '글자 색상 : [color=색상]텍스트[/color]';
+$lang['Font size : [size=X]text']['/size'] = '글자 크기 : [size=X]텍스트[/size]';
+$lang['Green'] = '녹색';
+$lang['Grey'] = '회색';
+$lang['Italic : [i]italic']['/i'] = '이탤릭 : [i]이탤릭[/i]';
+$lang['List element : [li]element']['/li'] = '목록 항목 : [li]항목[/li]';
+$lang['Orange'] = '주황색';
+$lang['Ordered list : [ol][li]element[/li]']['/ol'] = '순서 목록 : [ol][li]항목[/li][/ol]';
+$lang['Paragraph : [p]Paragraph']['/p'] = '단락 : [p]단락[/p]';
+$lang['Picture : [img]picture']['/img'] = '사진 : [img]사진[/img]';
+$lang['Purple'] = '보라색';
+$lang['Quote : [quote]quote']['/quote'] = '인용문 : [quote]인용문[/quote]';
+$lang['Red'] = '빨간색';
+$lang['Right : [right]right']['/right'] = '오른쪽 : [right]오른쪽[/right]';
